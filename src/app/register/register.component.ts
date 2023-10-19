5bff10fbaec2f5de08bc204a43eae35170756638
glenwery <glenwery14@gmail.com>
2023-10-19 12:04:38 +0700

    angular222102522

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,13 @@
+import { Component, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  constructor(private renderer: Renderer2) {
+    this.renderer.addClass(document.body, "Register-page")
+  }
+
+}
